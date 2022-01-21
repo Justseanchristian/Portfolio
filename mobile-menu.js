f49a98525a927aa4092ff41888ef1afa991afb34
Justseanchristian <97652111+Justseanchristian@users.noreply.github.com>
2022-01-21 09:59:38 -0800

    Add files via upload

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,11 +1,23 @@
-$(document).ready(function(){ 
-  $(".menu-container").click(function() {
-    $(".menu").addClass("toggled");
-  });
-  }); 
+// $(document).ready(function(){ 
+//   $(".menu-container").click(function() {
+//     $(".menu").addClass("toggled");
+//   });
+//   }); 
 
-  $(document).ready(function(){ 
-    $(".close-menu").click(function() {
-      $(".menu").removeClass("toggled");
-    });
-    }); 
+//   $(document).ready(function(){ 
+//     $(".close-menu").click(function() {
+//       $(".menu").removeClass("toggled");
+//     });
+//     }); 
+
+$(document).ready(function(){ 
+  $(".fbars").click(function() {
+ $("div.menu").addClass("toggled");
+  });
+  $(".close-menu").click(function() {
+   $("div.menu").removeClass("toggled");
+   });
+//  $(".close-menu").click(function() {
+//   $("div.menu").removeClass("toggled");
+//    });
+ }); 
